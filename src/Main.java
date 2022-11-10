f557bd28373b24799d7bcf8a76fcf367a343bb40
Ilya Aleshin <n3bovv@gmail.com>
2022-11-10 14:27:05 +0300

    add new change Change1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Check changes!");
+int a = 113;
+        int b = 56;
+        System.out.println(a - b);
     }
 }
